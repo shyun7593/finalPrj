5b0850d8c0dc22e0c193eb47dd55d6f2b0079516
최 승현 <seunghyun7593@gmail.com>
2025-04-21 15:56:13 +0900

    .

--- www/phptest.php
+++ www/phptest.php
@@ -1,7 +1,7 @@
 <?php
 
 // require '../vendor/autoload.php';
-include('../www/vendor/autoload.php');
+include('./vendor/autoload.php');
 include_once('./_common.php');
 
 
@@ -11,7 +11,7 @@ $client->setScopes([
     Google_Service_Sheets::SPREADSHEETS_READONLY  // 읽기 전용 권한
 ]);
 // $client->setAuthConfig('/api/spreadsheet-457500-2c59f048e424.json');
-$client->setAuthConfig('../www/api/spreadsheet-457500-2c59f048e424.json');
+$client->setAuthConfig('./api/spreadsheet-457500-2c59f048e424.json');
 $client->setAccessType('offline');
 
 $service = new Google_Service_Sheets($client);
@@ -25,30 +25,41 @@ $range9 = '표백등!N3:R'; // 9월
 $range0 = '표백등!T3:X'; // 가채점
 $range1 = '표백등!Z3:AD'; // 수능
 
-function getCodeMap() {
-    static $map = null;
-    if ($map === null) {
-        $result = sql_query("SELECT code, codeName FROM g5_cmmn_code WHERE code like '%C200%' AND depth = 2 AND upperCode not like '%C2005%'");
+// function getCodeMap() {
+//     static $map = null;
+//     if ($map === null) {
+//         $result = sql_query("SELECT code, codeName FROM g5_cmmn_code WHERE code like '%C200%' AND depth = 2 AND upperCode not like '%C2005%'");
+//         var_dump($result);
+//         $map = [];
+//         while ($rows = sql_fetch_array($result)) {
+//             $map[$rows['codeName']] = $rows['code'];
+//         }
+//     }
 
-        $map = [];
-        while ($rows = sql_fetch_array($result)) {
-            $map[$rows['codeName']] = $rows['code'];
-        }
-    }
+//     return $map;
+// }
 
-    return $map;
-}
-
-function subjectCode($subject) {
-    $map = getCodeMap();
-    return isset($map[$subject]) ? $map[$subject] : $subject;
-}
+// function subjectCode($subject) {
+//     $map = getCodeMap();
+//     return isset($map[$subject]) ? $map[$subject] : $subject;
+// }
 
 
 // 3월
 echo '3월<br>';
-$response3 = $service->spreadsheets_values->get($spreadsheetId, $range3);
-$values3 = $response3->getValues();
+try {
+    $response3 = $service->spreadsheets_values->get($spreadsheetId, $range3);
+    $values3 = $response3->getValues();
+
+    echo "<pre>";
+    print_r($values3);
+    echo "</pre>";
+} catch (Exception $e) {
+    echo '오류 발생: ' . $e->getMessage();
+}
+
+// $response3 = $service->spreadsheets_values->get($spreadsheetId, $range3);
+// $values3 = $response3->getValues();
 
 
 
@@ -57,34 +68,36 @@ if (empty($values3)) {
 } else {
     // $prevSub = '';
     foreach ($values3 as $row) {
-        
-        $subCode = subjectCode($row[0]);
-        sql_query("INSERT INTO g5_gradeCut set
-            gradeYear = '2025',
-            gradeCode = '{$subCode}',
-            gradeScore = '{$row[1]}',
-            gradePscore = '{$row[2]}',
-            gradeSscore = '{$row[3]}',
-            gGrade = '{$row[4]}',
-            regId = '{$regId}',
-            gradeType = 'm_3'
-        ");
-        if(strstr($subCode,'C2004')){
-            $subCode2 = str_replace('C2004', 'C2005', $subCode);
-            sql_query("INSERT INTO g5_gradeCut set
-                gradeYear = '2025',
-                gradeCode = '{$subCode2}',
-                gradeScore = '{$row[1]}',
-                gradePscore = '{$row[2]}',
-                gradeSscore = '{$row[3]}',
-                gGrade = '{$row[4]}',
-                regId = '{$regId}',
-                gradeType = 'm_3'
-            ");
-        }
+        echo '여기';
+        echo $row.'<br>';
+        // $subCode = subjectCode($row[0]);
+        // echo $subCode;
+        // sql_query("INSERT INTO g5_gradeCut set
+        //     gradeYear = '2025',
+        //     gradeCode = '{$subCode}',
+        //     gradeScore = '{$row[1]}',
+        //     gradePscore = '{$row[2]}',
+        //     gradeSscore = '{$row[3]}',
+        //     gGrade = '{$row[4]}',
+        //     regId = '{$regId}',
+        //     gradeType = 'm_3'
+        // ");
+        // if(strstr($subCode,'C2004')){
+        //     $subCode2 = str_replace('C2004', 'C2005', $subCode);
+        //     sql_query("INSERT INTO g5_gradeCut set
+        //         gradeYear = '2025',
+        //         gradeCode = '{$subCode2}',
+        //         gradeScore = '{$row[1]}',
+        //         gradePscore = '{$row[2]}',
+        //         gradeSscore = '{$row[3]}',
+        //         gGrade = '{$row[4]}',
+        //         regId = '{$regId}',
+        //         gradeType = 'm_3'
+        //     ");
+        // }
     }
 }
-
+exit;
 // 6월
 echo '<br>6월<br>';
 $response6 = $service->spreadsheets_values->get($spreadsheetId, $range6);
