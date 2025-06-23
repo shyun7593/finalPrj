ddbe498e9de13ea3e57ce5cd1b47fa07d8903c8b
최 승현 <seunghyun7593@gmail.com>
2025-06-23 10:49:55 +0900

    .

--- www/bbs/inteCollege_update.php
+++ www/bbs/inteCollege_update.php
@@ -8,7 +8,7 @@ $id = $_POST['id'];
 $type = $_POST['type'];
 
 if($type == 'add'){
-    $result = sql_query("INSERT INTO g5_add_college SET subIdx = '{$idx}', memId = '{$id}'");
+    $result = sql_query("INSERT INTO g5_add_college SET subIdx = '{$idx}', memId = '{$id}',regId = '{$_SESSION['ss_mb_id']}'");
 
 } else if($type == 'remove'){
     $result = sql_query("DELETE FROM g5_add_college WHERE memId = '{$id}' AND subIdx = '{$idx}'");
