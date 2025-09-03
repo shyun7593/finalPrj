d830db8807036880e733cafbdc098397efe96fe3
최 승현 <seunghyun7593@gmail.com>
2025-09-03 10:24:06 +0900

    .

--- www/theme/basic/shop/shop.head.php
+++ www/theme/basic/shop/shop.head.php
@@ -115,7 +115,7 @@ add_stylesheet('<link rel="stylesheet" href="'.G5_JS_URL.'/owlcarousel/owl.carou
                 $menus= sql_query("SELECT * FROM g5_menu WHERE me_code='{$_SESSION['mb_profile']}' AND me_use = 1 ORDER BY me_order");
                 foreach($menus as $mu => $m){
             ?>
-                <li style="width: 100%;cursor:pointer;" class="<?if($m['me_link'] == $nowUrl) echo ' active';?>"><a style="padding:12px 0;display: flex;justify-content: center;align-items: center;" href="<?= $m['me_link'] ?>"><?if($m['me_link'] == 'notice') echo $newNotice;?><?= $m['me_name']?></a></li>
+                <li style="width: 100%;cursor:pointer;" class="<?if($m['me_link'] == $nowUrl) echo ' active';?>"><a style="padding:12px 0;display: flex;justify-content: center;align-items: center;" href="<?= $m['me_link'] ?>.php"><?if($m['me_link'] == 'notice') echo $newNotice;?><?= $m['me_name']?></a></li>
             <?}?>
             <li style="width: 100%;cursor:pointer;" class="<?if('myInfo' == $nowUrl) echo ' active';?>"><a style="padding:12px 0;display: flex;justify-content: center;align-items: center;" href="myInfo">내 정보</a></li>
         </ul>
